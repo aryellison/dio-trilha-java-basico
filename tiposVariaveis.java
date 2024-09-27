c430666ada589cd6de24c37ccf4e72e6b85e32f7
Aryellison Pontes <142668000+aryellison@users.noreply.github.com>
2024-09-27 13:55:39 -0300

    Aula de tipos primitivos e variáveis

---
+++ tiposVariaveis.java
@@ -0,0 +1,19 @@
+public class tiposVariaveis {
+    public static void main(String[] args) throws Exception {
+        //TIPOS PRIMITIVOS
+        //ESTUDAR A CLASSE STRING QUE REPRESENTA TEXTO NA APLICAÇÃO JAVA
+    double salarioMinino = 2500;
+
+    short numeroCurto = 1;
+    int numeroNormal = numeroCurto;
+    short numeroCurto2 = (short) numeroNormal;
+
+    int numero = 5;
+
+    numero = 10;
+    System.out.println(numero);
+
+    final double VALOR_DE_PI = 3.14;
+    System.out.println(VALOR_DE_PI);
+    }
+}
